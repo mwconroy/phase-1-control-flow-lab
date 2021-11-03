d06d97c7550cf097d9e5bbef6db6c7affc967ece
Mike <mikewconroy3@gmail.com>
2021-11-03 15:41:42 -0500

    commit tree f98dfa262c429dd8ae7f00559971645d01ba8085

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+     function scuberGreetingForFeet(someNumber) {
+      let result
+      if (someNumber <= 400) {
+        result = 'This one is on me!';
+      } else if( someNumber < 2499)
+          result = 'I will gladly take your thirty bucks.'; 
+        else if (someNumber > 2500){
+            result = 'No can do.';
+          }
+          return result
+       }
+        
+    function ternaryCheckCity(city){
+      return city === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+    function switchOnCharmFromTip(tip){
+      let response;
+      switch (tip){
+        case 'generous':
+          response = 'Thank you so much.'
+          break;
+        case 'not as generous':
+          response = 'Thank you.'
+          break;
+        case 'thanks for everything':
+          response = 'Bye.'
+          break; 
+      }
+     return response
 }
